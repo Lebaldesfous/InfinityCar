da8f8b9f17891315e9892a7532d06b0053b8f6a5
Lebaldesfous <guillaume-huet@hotmail.com>
2023-04-04 10:30:21 +0200

    Users

--- src/main/java/com/location/car/model/User.java
+++ src/main/java/com/location/car/model/User.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity // This tells Hibernate to make a table out of this class
-@Table(name = "User")
+@Table(name = "account")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +19,39 @@ public class User {
     @Column(name = "username")
     @NonNull
     private String username;
+
+    @Column(name = "password")
+    @NonNull
+    private String password;
+
+    @Column(name = "email")
+    @NonNull
+    private String email;
+
+   
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password=password;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+
 }
--- src/main/java/com/location/car/repository/UserRepository.java
+++ src/main/java/com/location/car/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.location.car.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.location.car.model.Car;
+import com.location.car.model.User;
+
+import java.util.Optional;
 
 
-public interface UserRepository extends CrudRepository<Car, Integer>{}
+public interface UserRepository extends CrudRepository<User, Integer>{
+    public Optional<User> findUserByEmail(String email);
+}
