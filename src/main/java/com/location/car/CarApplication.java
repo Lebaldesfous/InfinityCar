33331c48193da6a0aaf1101d0c551850e7f39369
Florian Grimmer <grimmer.florian@outlook.fr>
2023-04-06 09:05:38 +0200

    Modification du back

--- src/main/java/com/location/car/CarApplication.java
+++ src/main/java/com/location/car/CarApplication.java
@@ -3,10 +3,6 @@ package com.location.car;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @SpringBootApplication
 public class CarApplication {
@@ -15,23 +11,4 @@ public class CarApplication {
 		SpringApplication.run(CarApplication.class, args);
 	}
 	
-	private static final AntPathRequestMatcher[] WHITE_LIST_URLS = {
-		new AntPathRequestMatcher("/**"),
-		
-	};
-
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-		http
-				.authorizeRequests(authorizeRequests ->
-						authorizeRequests
-								.requestMatchers(WHITE_LIST_URLS).permitAll()
-
-				)
-				.formLogin(AbstractHttpConfigurer::disable)
-				.csrf(AbstractHttpConfigurer::disable);
-
-		return http.build();
-	}
 }
