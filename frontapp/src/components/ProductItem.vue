2156cecc16aa021e6898170e501a60cdeaeb4c16
Florian Grimmer <grimmer.florian@outlook.fr>
2023-04-12 10:03:47 +0200

    Modification de la page Home et des produits disponibles

--- frontapp/src/components/ProductItem.vue
+++ frontapp/src/components/ProductItem.vue
@@ -1,20 +1,24 @@
 <template>
     <div class="col-sm-12 col-md-4 mb-4">
         <div class="card custom-card">
-            <img src="https://imgur.com/edOjtEC.png" class="card-img-top" width="100%">
+            <img :src="car.image" class="card-img-top" width="100%">
             <div class="card-body pt-0 px-0">
                 <div class="d-flex flex-row justify-content-between mb-0 px-3">
+                    <small class="text-muted mt-1">CAR</small>
+                    <h6>{{car.name}}</h6>
+                </div>
+                <div class="d-flex flex-row justify-content-between mb-0 px-3">
                     <small class="text-muted mt-1">STARTING AT</small>
-                    <h6>&dollar;22,495&ast;</h6>
+                    <h6>&dollar;{{car.price}}</h6>
                 </div>
                 <hr class="mt-2 mx-3">
                 <div class="d-flex flex-row justify-content-between px-3 pb-4">
                     <div class="d-flex flex-column">
                         <span class="text-muted">Fuel Efficiency</span>
-                        <small class="text-muted">L/100KM&ast;</small>
+                        <small class="text-muted">L/100KM</small>
                     </div>
                     <div class="d-flex flex-column">
-                        <h5 class="mb-0">8.5/7.1</h5>
+                        <h5 class="mb-0">{{ car.fuel_efficiency }}</h5>
                         <small class="text-muted text-right">(city/Hwy)</small>
                     </div>
                 </div>
@@ -24,8 +28,7 @@
                         <div class="d-flex flex-row">
                             <img src="https://imgur.com/iPtsG7I.png" width="35px" height="25px">
                             <div class="d-flex flex-column ml-1">
-                                <small class="ghj">1.4L MultiAir</small>
-                                <small class="ghj">16V I-4 Turbo</small>
+                                <small class="ghj">{{ car.engine }}</small>
                             </div>
                         </div>
                     </div>
@@ -33,40 +36,46 @@
                         <small class="text-muted mb-2">HORSEPOWER</small>
                         <div class="d-flex flex-row">
                             <img src="https://imgur.com/J11mEBq.png">
-                            <h6 class="ml-1">135 hp&ast;</h6>
+                            <h6 class="ml-1">{{car.horsepower}}</h6>
                         </div>
                     </div>
                 </div>
                 <small class="text-muted key pl-3">Standard key Features</small>
                 <div class="mx-3 mt-3 mb-2"><button type="button" class="btn btn-danger btn-block"
-                        @click="showModal = true"><small>BUILD & PRICE</small></button></div>
+                        @click="showModal = true"><small>BOOK</small></button></div>
                 <small class="d-flex justify-content-center text-muted">*Legal Disclaimer</small>
             </div>
         </div>
     </div>
     <div class="modal" tabindex="-1" role="dialog" style="display: block;" v-if="showModal">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Rent confirmation</h5>
-            <button type="button" class="close" @click="showModal = false">
-              <span>&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            Are you sure you want to book this vehicle? This will appear on your profile page and decrease available stock.
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary text-danger" @click="showModal = false">Cancel</button>
-            <button type="button" class="btn btn-primary text-white bg-danger">Rent</button>
-          </div>
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Book confirmation</h5>
+                    <button type="button" class="close" @click="showModal = false">
+                        <span>&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to book this vehicle? This will appear on your profile page and decrease available
+                    stock.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary text-danger" @click="showModal = false">Cancel</button>
+                    <button type="button" class="btn btn-primary text-white bg-danger">Rent</button>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
 </template>
 
 <script>
 export default {
+    props: {
+        car: {
+            type: Object
+        }
+    },
     data() {
         return {
             showModal: false,
@@ -103,5 +112,10 @@ export default {
 
 .custom-card {
     width: 250px;
+
+}
+
+.card-img-top {
+    height: 170px;
 }
 </style>
